b0cce6fb4666959d34cd78d4b156fc4807c9c6b9
Evgenia Batyrina <evgenia.batyrina@netwrix.com>
2018-05-11 19:05:39 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@
  */
 function createDivWithText(text) {
     var div = document.createElement('div'); 
+
     div.innerText = text;
 
     return div;
@@ -23,7 +24,8 @@ function createDivWithText(text) {
  Функция должна вставлять элемент, переданный в пaрaметре what в начало элемента, переданного в параметре where
 
  Пример:
-   prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
+   prepend(document.querySelector('#one'), document.querySelector('#two')) 
+   // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
  */
 function prepend(what, where) {
     where.insertBefore(what, where.firstChild);
@@ -34,7 +36,8 @@ function prepend(what, where) {
 
  3.1: Функция должна перебрать все дочерние элементы узла, переданного в параметре where
 
- 3.2: Функция должна вернуть массив, состоящий из тех дочерних элементов следующим соседом которых является элемент с тегом P
+ 3.2: Функция должна вернуть массив, состоящий из тех дочерних элементов 
+ следующим соседом которых является элемент с тегом P
 
  Пример:
    Представим, что есть разметка:
@@ -46,13 +49,14 @@ function prepend(what, where) {
       <p></p>
    </body>
 
-   findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span т.к. следующим соседом этих элементов является элемент с тегом P
+   findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span 
+   т.к. следующим соседом этих элементов является элемент с тегом P
  */
 function findAllPSiblings(where) {
     var wh = where.children,
         newarr = [];
 
-	for (const elem of wh) {
+    for (const elem of wh) {
         if (elem.nextElementSibling.nodeName === 'P') {
             newarr.push(elem);
         }
@@ -64,7 +68,8 @@ function findAllPSiblings(where) {
 /*
  Задание 4:
 
- Функция представленная ниже, перебирает все дочерние узлы типа "элемент" внутри узла переданного в параметре where и возвращает массив из текстового содержимого найденных элементов
+ Функция представленная ниже, перебирает все дочерние узлы типа "элемент" внутри узла 
+ переданного в параметре where и возвращает массив из текстового содержимого найденных элементов
  Но похоже, что в код функции закралась ошибка и она работает не так, как описано.
 
  Необходимо найти и исправить ошибку в коде так, чтобы функция работала так, как описано выше.
@@ -113,7 +118,8 @@ function deleteTextNodes(where) {
 /*
  Задание 6:
 
- Выполнить предудыщее задание с использование рекурсии - то есть необходимо заходить внутрь каждого дочернего элемента (углубляться в дерево)
+ Выполнить предудыщее задание с использование рекурсии - то есть необходимо заходить внутрь каждого дочернего элемента 
+ (углубляться в дерево)
 
  Задачу необходимо решить без использования рекурсии, то есть можно не уходить вглубь дерева.
  Так же будьте внимательны при удалении узлов, т.к. можно получить неожиданное поведение при переборе узлов
@@ -123,14 +129,16 @@ function deleteTextNodes(where) {
    должно быть преобразовано в <span><div><b></b></div><p></p></span>
  */
 function deleteTextNodesRecursive(where) {
-	var wh = where.childNodes;
+    for (var i=0; i < where.childNodes.length; i++) {
+        var elem = where.childNodes[i];
 
-    for (const elem of wh) {
         if (elem.nodeType === 3) {
             where.removeChild(elem);
-        } else if (elem.nodeType === 1){
+            i--;
+        } else if (elem.nodeType === 1) {
             deleteTextNodesRecursive(elem);
-	}
+        }
+    }
 }
 
 /*
